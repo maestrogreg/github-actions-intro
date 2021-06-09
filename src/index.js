461483645c1e0bfd210be7b36177986f63dea923
= <=>
2021-06-09 02:32:38 +0100

    added futuramaapi api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const core = require('@actions/core');
 
 async function start(){
+    const character = core.getInput('character');
+    console.log(character);
+
     const result = await axios.get('https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1');
     const { data } = result;
     const value = data[0];
